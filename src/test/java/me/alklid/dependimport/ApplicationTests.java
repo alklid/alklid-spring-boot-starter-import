07aad4ee631db4d09129c5ad33e104436a76e77c
alklid <alklid@gmail.com>
2019-01-12 22:51:53 +0900

    initial commit

---
+++ src/test/java/me/alklid/dependimport/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alklid.dependimport;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+
+}
+
